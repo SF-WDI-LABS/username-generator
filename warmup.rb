25a438b82b331199e6faab2be38464ac7620cc5d
balbini <balbini91@gmail.com>
2017-09-12 15:56:25 -0700

    warmup complete

--- warmup.rb
+++ warmup.rb
@@ -1,17 +1,22 @@
 # Run `rspec spec/the_warmup_spec.rb` to get started.
 
 def say_hello
-  "hi"
+  "hello"
 end
 
 def scream(message)
-  message
+  message = "boo"
+  message.upcase + "!"
 end
 
-def first_char
-  "z"
+def first_char(input)
+  input[0].downcase
 end
 
-def polly_wanna
-  "crackercrackercracker"
+def polly_wanna(word)
+    word * 4
+end
+
+def after_you
+  "no, after you"
 end
