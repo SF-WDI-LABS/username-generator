89f270ebc3b19f3b603426f7638514e469fa9abf
jcheng305 <jcheng305@MacBook-Pro-3.local>
2017-09-12 16:47:56 -0700

    Completed Warmup, and currently working on format_year

--- username.rb
+++ username.rb
@@ -2,7 +2,20 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+
+if first.length == 0
+ p nil
+elsif last.length == 0
+ p nil
+else
+  first_name = first.delete(" ")
+  last_name = last.delete(" ")
+  first_inlast = first_name[0]
+  full_name = first_inlast + last_name
+  lowercase_name = full_name.delete(" ").downcase
+  # remove_allcharacters = full_name.gsub(/[^a-zA-Z0-9\-]/,"")
+  p lowercase_name
+  end
 end
 
 def format_year
