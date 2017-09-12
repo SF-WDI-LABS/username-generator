877e6428860c8e9d9a088e4461ec68d17547b247
conmart <connor.a.martinelli@gmail.com>
2017-09-12 16:15:46 -0700

    format_name works

--- username.rb
+++ username.rb
@@ -2,7 +2,12 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+  if (first == "" || last == "")
+    nil
+  else
+  (first.gsub(/\s+/, "")[0] + last.gsub(/\s+/, "")).downcase
+  # (first.gsub(/[^a-z]/, "")[0] + last.gsub(/[^a-z]/, "")).downcase
+  end
 end
 
 def format_year
