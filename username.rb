670803ddf981aec897b3584e60f3c5a9337d7b9c
supertrunkes <supertrunkes@gmail.com>
2017-09-12 18:20:36 -0700

    realized i havent been comminting finished levels one and two

--- username.rb
+++ username.rb
@@ -1,14 +1,48 @@
 # Make sure to run the tests in your /spec folder
-# Run `rspec /spec/username_spec.rb` to get started.
+# Run `rspec spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+  if last == "" || first == ""
+    return nil
+  else
+    return first.downcase.strip[0] + last.downcase.delete(' ')
+  end
 end
 
-def format_year
-  nil
+def format_year(year)
+  if year.to_s.length != 4
+    return nil
+  else
+    year.to_s[-2..-1]
+  end
 end
 
-def build_username
-  nil
+
+def check_privilege(integer = 0)
+  user_types =["user", "seller", "manager", "admin"]
+  if integer.floor == 1
+    return user_types[1]
+  elsif integer.floor == 2
+    return user_types[2]
+  elsif integer.floor >= 3
+    return user_types[3]
+  else
+    return user_types[0]
+  end
+end
+
+def user_type_prefix(integer)
+  if check_privilege(integer) == "seller"
+    return "seller-"
+  elsif check_privilege(integer) == "manager"
+    return "manager-"
+  elsif check_privilege(integer) == "admin"
+    return "admin-"
+  else
+    return ""
+  end
+end
+
+def build_username(first, last, year, privilege_level = 0)
+user_type_prefix(privilege_level)+  format_name(first, last) + format_year(year)
 end
