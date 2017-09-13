d5c3d73722b65da62e6989cd3ea2e47143d40c25
ellismitchell <elligmitchell@gmail.com>
2017-09-13 09:04:00 -0700

    finished hw

--- warmup.rb
+++ warmup.rb
@@ -1,17 +1,25 @@
 # Run `rspec spec/the_warmup_spec.rb` to get started.
 
 def say_hello
-  "hi"
+  "hello"
 end
 
 def scream(message)
-  message
+  message.upcase+'!'
 end
 
-def first_char
-  "z"
+def first_char string
+  string.downcase[0]
 end
 
-def polly_wanna
-  "crackercrackercracker"
+def polly_wanna string
+	str = ""
+  3.times do
+  	str += string
+  end
+  str
+end
+
+def after_you 
+	"no, after you"
 end
