5d2bee8d67cd2fbacf4fa96870f9c40f144e70f5
BryanReese <bryan_reese209@yahoo.com>
2017-09-12 20:06:44 -0700

    format-name

--- username.rb
+++ username.rb
@@ -2,13 +2,65 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+if first.length<1 || last.length<1
+	nil
+else
+  first = first.split.join
+  last = last.split.join
+  str = (first[0]+last).downcase
+end
 end
 
-def format_year
-  nil
+
+
+
+def check_privilege(type)
+
+if type.floor == 1
+	"seller"
+
+elsif type.floor == 2
+	"manager"
+
+elsif type.floor == 3 
+	"admin"
+
+else 
+ 	"user"
+
+	end
 end
+
+
+
+def user_type_prefix(type)
+	if type.floor == 1
+	"seller"+"-"
+
+elsif type.floor == 2
+	"manager"+"-"
+
+elsif type.floor == 3 
+	"admin"+"-"
+
+else 
+ 	"user"+"-"
+
+	end
+end
+
+
+
+
+
+
+def format_year(year)
+	year.to_s.length == 4 ? year.to_s.slice(-2,2) : nil
+end
+
+
+
 
 def build_username
-  nil
+	nil
 end
